a497a6e7cf2d7a5d67434b10ee41796e0b7d9de3
Kien Pham <747782@student.tor.loc>
2019-08-07 17:14:36 -0400

    second commit of the day

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 var express = require('express');
 var app = express();
-app.get('/',function(req,res){
-res.send('Hello World')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(function(req,res,next){
+    console.log("Iam in the middle");
+    console.log(req.body);
+    next();
 })
 
+app.get('/',function(req,res){
+    console.log(req.body)
+res.send('<h1>Hello World</h1>')
+})
+app.post('/',function(req,res){
+    console.log(req.body)
+    res.send('POST sent')
+    })
+app.put('/',function(req,res){
+    res.send('PUT sent')
+    })
+app.delete('/',function(req,res){
+    res.send('DELETE sent')
+    })
 app.listen(3000)
