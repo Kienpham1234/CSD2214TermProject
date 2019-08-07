26e184cce5a869ef30d55ddd8efb951996236c86
Kien Pham <747782@student.tor.loc>
2019-08-07 16:34:38 -0400

    first commit of the day

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-const http = require('http');
-const data = {
-firstName:'Laurence',
-lastName :'Svekis'
-}
-
-const site = http.createServer(function(req,res){
-console.log('Hello World');
-console.log(req.headers);
-console.log(req.url);
-res.setHeader('Content-Type','application/json');
-//res.setHeader('Content-Type','text/html');
-//res.write('fh');
-res.write(JSON.stringify(data)); 
-res.end();
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+res.send('Hello World')
 })
-site.listen(3000);
+
+app.listen(3000)
